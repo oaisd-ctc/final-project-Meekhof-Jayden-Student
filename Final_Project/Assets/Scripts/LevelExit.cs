4aea7829afc5e691c59e1868d1cac321b2043e63
Meekhof-Jayden-Student <jame1ea@student.oaisd.org>
2023-05-15 09:56:31 -0400

    .

---
+++ Final_Project/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+}
